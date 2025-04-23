041306c3b17f2d75b7f4004eb9a3a60ff9bf71f1
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-04-23 17:11:34 +0200

    fix movie details

--- src/Components/Recommendations/Recommendations.jsx
+++ src/Components/Recommendations/Recommendations.jsx
@@ -34,7 +34,7 @@ export const Recommendations = () => {
     <h2>Recommendations</h2>
     <div className="list-grid">
       {movies.map((movie) => (
-        <Link to={`/movie/${movie.id}`} key={movie.id} className="movie-card-link">
+        <Link to={`/movie-details/${movie.id}`} key={movie.id} className="movie-card-link">
           <div className="movie-card">
             <div className="poster-wrapper">
               <img
